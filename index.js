78feeb31a6f11a14ff207402f8aa2c08d7e7f23b
guilherme <gsvaldevieso@gmail.com>
2015-10-04 23:28:40 -0300

    Now balls respect the screen limits

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-var x = 100, y =75, operatorX = 2, operatorY = 2;
 var Circles = [];
-
-var Circle = {
-  posX: Math.random() * 100,
-  posY: 75,
-  operatorX: 2,
-  operatorY: 2
-};
-
-var colors = ['green','red','silver','blue','yellow','black','pink'
-];
-
+var colors = ['green','red','silver','blue','yellow','black','pink'];
+var RADIUS_SIZE = 30;
+var MAX_BALLS_IN_SCREEN = 20;
 var drawCircle = function(){
-
   var c=document.getElementById("canvas");
   var ctx=c.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if(Circles.length < 100 && Math.floor((Math.random() * 100)) == 0)
-      Circles.push({posX: Math.random()*100,posY: 75,operatorX:2,operatorY:2, colorize: colors[Math.floor(Math.random() * 7)]});
+    if(Circles.length < MAX_BALLS_IN_SCREEN && Math.floor((Math.random() * 100)) == 0)
+      Circles.push({posX: Math.floor((Math.random())*100)+RADIUS_SIZE,
+		    posY: 75,
+		    operatorX:2 * Math.random(),
+		    radius:  RADIUS_SIZE,
+		    operatorY:2 *  Math.random(),
+		    colorize: colors[Math.floor(Math.random() * 7)]});
 
   for(var i=0; i < Circles.length; i++){
     ctx.beginPath();
     Circles[i].posX = Circles[i].posX + Circles[i].operatorX;
     Circles[i].posY = Circles[i].posY + Circles[i].operatorY;
-    ctx.arc(Circles[i].posX,Circles[i].posY,50,0,2*Math.PI);
+    ctx.arc(Circles[i].posX,Circles[i].posY,Circles[i].radius,0,2*Math.PI);
     ctx.fillStyle = Circles[i].colorize;
-     ctx.fill();
+    ctx.fill();
     ctx.stroke();
 
-    if(Circles[i].posX > c.width){
+    if((Circles[i].posX+Circles[i].radius) > c.width){
       Circles[i].operatorX = Circles[i].operatorX * -1;
-    }else if(Circles[i].posX < 0){
+    }else if((Circles[i].posX-Circles[i].radius) < 0){
       Circles[i].operatorX = Circles[i].operatorX * -1;
     }
 
-    if(Circles[i].posY > c.height){
+    if((Circles[i].posY+Circles[i].radius) > c.height){
       Circles[i].operatorY = Circles[i].operatorY * -1;
-    }else if(Circles[i].posY < 0){
+    }else if((Circles[i].posY-Circles[i].radius) < 0){
       Circles[i].operatorY = Circles[i].operatorY * -1;
     }
   }
 };
 
-$(document).ready(function(){
 
-  setInterval(drawCircle, 10);
+$(document).ready(function(){
+    var canvas = $('#canvas');
+    canvas.attr('width',$(window).width());
+    canvas.attr('height',$(window).height());
+    setInterval(drawCircle, 5);
 });
